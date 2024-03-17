f942c4c8609bd494023280e20e3cbbab7812668d
AT Sowell <atsowell@gmail.com>
2024-03-17 01:17:46 -0400

    test

--- pcapcopier/tmp/installer.sh
+++ pcapcopier/tmp/installer.sh
@@ -2,9 +2,9 @@
 version='2024-03-16-a'
 DATED=$(date +%Y%m%d-%H%M%S)
 
-python3 ./tmp/pipinstaller.py
+python3 ./pipinstaller.py
 
-cp ./tmp/pcapcopier.py /usr/local/sbin/pcapcopier
+cp ./pcapcopier.py /usr/local/sbin/pcapcopier
 chmod a+x /usr/local/sbin/pcapcopier
 ls -alh /usr/local/sbin/pcapcopier
 
