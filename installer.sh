347ef91981f7808105750c2f71e44412f10d4fba
AT Sowell <atsowell@gmail.com>
2024-03-17 00:27:17 -0400

    maybe working

--- installer.sh
+++ installer.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 version='2024-03-16-a'
+DATED=$(date +%Y%m%d-%H%M%S)
 
-ls -lh /tmp/pcapcopier/wheels/PyNaCl-1.5.0.whl
-ls -lh /tmp/pcapcopier/wheels/bcrypt-4.1.2.whl
-ls -lh /tmp/pcapcopier/wheels/cffi-1.16.0.whl
-ls -lh /tmp/pcapcopier/wheels/cryptography-42.0.5.whl
-ls -lh /tmp/pcapcopier/wheels/paramiko-3.4.0.whl
-ls -lh /tmp/pcapcopier/wheels/pycparser-2.21.whl
-ls -lh /tmp/pcapcopier/wheels/scp-0.14.5.whl
+pip3 install --no-index --no-deps tmp/wheels/bcrypt-4.1.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+pip3 install --no-index --no-deps tmp/wheels/PyNaCl-1.5.0-cp36-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl
+pip3 install --no-index --no-deps tmp/wheels/pycparser-2.21-py2.py3-none-any.whl
+pip3 install --no-index --no-deps tmp/wheels/scp-0.14.5-py2.py3-none-any.whl
+pip3 install --no-index --no-deps tmp/wheels/paramiko-3.4.0-py3-none-any.whl
+pip3 install --no-index --no-deps tmp/wheels/cffi-1.16.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+pip3 install --no-index --no-deps tmp/wheels/cryptography-42.0.5-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 
-#pip3 install /tmp/pcapcopier/wheels/PyNaCl-1.5.0.whl
-#pip3 install /tmp/pcapcopier/wheels/bcrypt-4.1.2.whl
-#pip3 install /tmp/pcapcopier/wheels/cffi-1.16.0.whl
-#pip3 install /tmp/pcapcopier/wheels/cryptography-42.0.5.whl
-#pip3 install /tmp/pcapcopier/wheels/paramiko-3.4.0.whl
-#pip3 install /tmp/pcapcopier/wheels/pycparser-2.21.whl
-#pip3 install /tmp/pcapcopier/wheels/scp-0.14.5.whl
+cp ./tmp/pcapcopier.py /usr/local/sbin/pcapcopier
+chmod a+x /usr/local/sbin/pcapcopier
+ls -alh /usr/local/sbin/pcapcopier
+
 
 cp /tmp/pcapcopier/pcapcopier.py /usr/local/sbin/pcapcopier
 exit 0
