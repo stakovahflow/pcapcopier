5180fe505a9b609b44f6064110a9acf0b00c59bd
AT Sowell <atsowell@gmail.com>
2024-03-18 22:50:28 -0400

    pexpect pxssh version 4.

--- pcapcopier-v4/pcapcopier.py
+++ pcapcopier-v4/pcapcopier.py
@@ -109,15 +109,15 @@ def ssh_session_remote_path(username,host,password):
         for line in lines:
             if re.match(r'[a-z]', line):
                 print(f"output: {line}")
-                getremotefilelist = f"find {line}/pcaps/continuous_capture/"
-            #print(line)
-            s.sendline (getremotefilelist)
-            s.prompt()
-            output = s.before.decode()
-            lines = output.splitlines()[1:]
-            for line in lines:
-                if re.match(r'[a-z]', line):
-                    print(f"Remote path: {line}")
+                get_remote_file_list_command = f'find -type f {line}/pcaps/continuous_capture/ -type f -name "*.pcap"'
+                logging.info(f"Attemptint to run command: {get_base_remote_path_command}")
+                s.sendline (get_remote_file_list_command)
+                s.prompt()
+                output = s.before.decode()
+                lines = output.splitlines()[1:]
+                for line in lines:
+                    if re.match(r'[a-z]', line):
+                        print(f"Remote path: {line}")
         s.logout()
     except pxssh.ExceptionPxssh as e:
         print("pxssh failed on login.")
